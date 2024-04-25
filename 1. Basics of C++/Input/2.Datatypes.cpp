0c0a0a91c0d11de9992bb1fc50db61f66f83f5b8
karahmon <monilkarania@gmail.com>
2024-04-25 10:02:38 +0530

    Learned Input/Output and Datatypes integers and decimals

---
+++ 1. Basics of C++/Input/2.Datatypes.cpp
@@ -0,0 +1,31 @@
+//Datatypes 
+
+//Integers:- There are three types of datatypes in integers : int(smallest range),long(bigger range then int),long long (highest range)
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x;
+    long y;
+    long long z;
+	cin >> x >> y >> z;
+    cout<<"Value of X: "<< x <<"\n";
+    cout<<"Value of Y: "<< y <<"\n";
+    cout<<"Value of Z: "<< z <<"\n";
+	return 0;
+}
+
+//Decimals:- There are three types of datatypes in integers : float (smallest range),double (bigger range then int),long double (highest range)
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    float x;
+    double y;
+    long double z;
+	cin >> x >> y >> z;
+    cout<<"Value of X: "<< x <<"\n";
+    cout<<"Value of Y: "<< y <<"\n";
+    cout<<"Value of Z: "<< z <<"\n";
+	return 0;
+}
