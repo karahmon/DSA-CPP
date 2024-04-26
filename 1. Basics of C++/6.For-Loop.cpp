39fef502d9f7cb5b8cd43ae9ae3608439f289517
karahmon <monilkarania@gmail.com>
2024-04-26 23:38:58 +0530

    Added code for Arrays, Strings, For Loop, and While Loop in C++

---
+++ 1. Basics of C++/6.For-Loop.cpp
@@ -0,0 +1,11 @@
+//Printing 5 times a sentence
+
+#include <iostream>
+using namespace std;
+
+int main() {
+   for (int i=0;i<5;i++){
+    cout<<"Hello World"<<"\n";
+   }
+    return 0;
+}
---
+++ 1. Basics of C++/7.While-loop.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+int main()
+
+{
+ int i=1;
+ while(i<=5){
+    cout<<"Hello"<<"\n";
+    i++;
+ }
+
+}
+
+//Do while - min one time execution
+#include<bits/stdc++.h>
+
+using namespace std;
+int main()
+
+{
+ int i=1;
+ do{
+    cout<<"Hello"<<"\n";
+    i++;
+ }while(i<=5);
+ return 0;
+
+}
